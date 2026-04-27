a6177f2e6f2044a1525858dd52ab2696c2869d8f
Bojin Li <git@bojin.li>
2026-04-28 08:25:21 +1000

    fix: use valid secure store auth key

--- apps/mobile/src/lib/pocketbase.ts
+++ apps/mobile/src/lib/pocketbase.ts
@@ -2,7 +2,7 @@ import { createPocketBaseClient } from '@infchat/pocketbase';
 import * as SecureStore from 'expo-secure-store';
 import { AsyncAuthStore } from 'pocketbase';
 
-const AUTH_STORAGE_KEY = 'infchat:pocketbase:auth';
+const AUTH_STORAGE_KEY = 'infchat.pocketbase.auth';
 
 const authStore = new AsyncAuthStore({
   initial: SecureStore.getItemAsync(AUTH_STORAGE_KEY),
